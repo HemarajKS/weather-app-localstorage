b6fd1e31bdc6749018ce8b5ff76250497db37ce1
HemarajKS <hemaraja.shettigar@robosoftin.com>
2022-11-26 15:16:31 +0530

    cleaned

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../components/header/header';
 import { NavLink } from 'react-router-dom';
 import Router from '../components/Router/Router';
@@ -6,6 +6,8 @@ import './home.css';
 import HomeBody from '../components/homeBody/homeBody';
 
 const Home = () => {
+  useEffect(() => {}, []);
+
   return (
     <div className="home">
       <Header />
